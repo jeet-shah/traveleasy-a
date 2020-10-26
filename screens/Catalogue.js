35f496ef008a5d8aa077af85628868691684f5ff
Jeet Shah <62886607+jeet-shah@users.noreply.github.com>
2020-10-26 14:56:48 +0530

    Add files via upload

---
+++ screens/Catalogue.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View,Image } from 'react-native';
+import {ListItem} from 'react-native-elements';
+
+const list = [
+    {
+    name: 'Shirt',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Cotton Shirts'
+  },
+  {
+    name: 'Pant',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Pants'
+  },
+]
+
+export default class Catalogue extends React.Component{
+
+    keyExtractor = (item, index) => index.toString()
+
+    renderItem = ({ item }) => {
+      return(
+        <ListItem
+        title={item.name}
+        subtitle={item.subtitle}
+        titleStyle={{ color: 'black', fontWeight: 'bold' }}
+        leftElement={
+          <Image style={{
+            width:50,
+            height:50
+          }} source={{uri:item.avatar_url}} />
+        }
+        
+      />
+  )}
+
+    render(){
+        return(
+            <View style={{backgroundColor:'black',flex:1}}>
+              <FlatList
+                keyExtractor={this.keyExtractor}
+                data={list}
+                renderItem={this.renderItem}
+              />
+            </View>
+        )
+    }
+}
